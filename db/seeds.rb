6cac82729c3ba6ce73c072cf37ea944001bc01b7
Hugo Lennep <hugovanlennep@gmail.com>
2023-09-13 16:49:03 +0200

    Completed yelp challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+wallys =  {name: "Wally's", address: "5 Newlands St, London", category: "french"}
+pepporoni_place =  {name: "Pepporoni Place", address: "20 Hydepark St, London", category: "italian"}
+noodle_king =  {name: "Noodle King", address: "52 Shoreditch High St, London", category: "japanese"}
+
+[dishoom, pizza_east, wallys, pepporoni_place, noodle_king].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
